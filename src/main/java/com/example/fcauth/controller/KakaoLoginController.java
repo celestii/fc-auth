f90598623fde0ca35dd1663958e8f91a4f8f51ac
celestii <lshh459@gmail.com>
2025-06-07 22:30:40 +0900

    [4-4] 확인된 정보가 등록된 유저의 정보인지 판단

--- src/main/java/com/example/fcauth/controller/KakaoLoginController.java
+++ src/main/java/com/example/fcauth/controller/KakaoLoginController.java
@@ -1,10 +1,8 @@
 package com.example.fcauth.controller;
 
-import com.example.fcauth.model.KakaoUserInfoRespDto;
-import com.example.fcauth.service.KakaoService;
+import com.example.fcauth.service.LoginService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,13 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RequiredArgsConstructor
 public class KakaoLoginController {
-    private final KakaoService kakaoService;
+    private final LoginService loginService;
 
     @GetMapping("/kakao/callback")
     public ResponseEntity callback(@RequestParam("code") String code) {
-        String token = kakaoService.getAccessTokenFromKakao(code);
-        KakaoUserInfoRespDto userInfo = kakaoService.getUserFromKakao(token);
-        log.info("userInfo " + userInfo.getKakaoAccount().getProfile().getNickName());
-        return new ResponseEntity(HttpStatus.OK);
+        return loginService.login(code);
     }
 }
---
+++ src/main/java/com/example/fcauth/service/LoginService.java
@@ -0,0 +1,26 @@
+package com.example.fcauth.service;
+
+import com.example.fcauth.model.KakaoUserInfoRespDto;
+import com.example.fcauth.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+    private final KakaoService kakaoService;
+    private final EmployeeRepository employeeRepository;
+
+    public ResponseEntity login(String code) {
+        String token = kakaoService.getAccessTokenFromKakao(code);
+        KakaoUserInfoRespDto userInfo = kakaoService.getUserFromKakao(token);
+        String nickName = userInfo.getKakaoAccount().getProfile().getNickName();
+        if (employeeRepository.existsByKakaoNickName(nickName)) {
+            return new ResponseEntity("환영합니다. " + nickName, HttpStatus.OK);
+        } else {
+            return new ResponseEntity("등록된 임직원이 아닙니다.", HttpStatus.FORBIDDEN);
+        }
+    }
+}
