9925a1835c2924f65008e96c59cdbc3f7ffeaae9
celestii <lshh459@gmail.com>
2025-06-07 22:17:10 +0900

    [4-3] 카카오 소셜 로그인 확인

--- src/main/java/com/example/fcauth/controller/KakaoLoginController.java
+++ src/main/java/com/example/fcauth/controller/KakaoLoginController.java
@@ -1,5 +1,8 @@
 package com.example.fcauth.controller;
 
+import com.example.fcauth.model.KakaoUserInfoRespDto;
+import com.example.fcauth.service.KakaoService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,10 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
+@RequiredArgsConstructor
 public class KakaoLoginController {
+    private final KakaoService kakaoService;
+
     @GetMapping("/kakao/callback")
     public ResponseEntity callback(@RequestParam("code") String code) {
-        log.info("code " + code);
+        String token = kakaoService.getAccessTokenFromKakao(code);
+        KakaoUserInfoRespDto userInfo = kakaoService.getUserFromKakao(token);
+        log.info("userInfo " + userInfo.getKakaoAccount().getProfile().getNickName());
         return new ResponseEntity(HttpStatus.OK);
     }
 }
